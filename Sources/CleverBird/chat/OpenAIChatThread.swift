e576d046aef2cbb668a2b215464adba8b3a56e17
B.T. Franklin <brandon.franklin@gmail.com>
2023-04-16 13:45:03 -0700

    Added a tokenCount() method to OpenAIChatThread

--- Sources/CleverBird/chat/OpenAIChatThread.swift
+++ Sources/CleverBird/chat/OpenAIChatThread.swift
@@ -57,6 +57,9 @@ public class OpenAIChatThread {
         messages.append(message)
     }
 
+}
+
+extension OpenAIChatThread {
     public func complete() async -> ChatCompletionResponse? {
 
         var urlComponents = URLComponents()
@@ -111,5 +114,42 @@ public class OpenAIChatThread {
             logger("Error executing request: \(error.localizedDescription)")
             return nil
         }
+    }}
+
+extension OpenAIChatThread {
+    public func tokenCount() -> Int {
+
+        let tokenEncoder: TokenEncoder
+        do {
+            tokenEncoder = try TokenEncoder()
+        } catch {
+            logger("Unable to create token encoder: \(error)")
+            return -1
+        }
+
+        var tokensPerMessage: Int
+
+        switch self.model {
+        case .gpt35Turbo:
+            tokensPerMessage = 4
+        case .gpt4:
+            tokensPerMessage = 3
+        }
+
+        var numTokens = 0
+        for message in messages {
+            do {
+                let roleTokens = try tokenEncoder.encode(text: message.role.rawValue).count
+                let contentTokens = try tokenEncoder.encode(text: message.content).count
+
+                numTokens += roleTokens + contentTokens + tokensPerMessage
+            } catch {
+                logger("Error encoding text: \(error)")
+            }
+        }
+
+        numTokens += 3  // every reply is primed with assistant
+
+        return numTokens
     }
 }
