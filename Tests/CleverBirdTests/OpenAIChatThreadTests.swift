954bd0c488b41317606bb64a74ffe025a10fde62
B.T. Franklin <brandon.franklin@gmail.com>
2023-04-15 23:51:05 -0700

    Added a unit test for the Chat Thread stuff.

---
+++ Tests/CleverBirdTests/OpenAIChatThreadTests.swift
@@ -0,0 +1,70 @@
+//  Created by B.T. Franklin on 4/15/23
+
+import XCTest
+@testable import CleverBird
+
+class OpenAIChatThreadTests: XCTestCase {
+
+    class MockURLRequester: URLRequester {
+        let response: JSONString
+        let logger: Logger
+
+        init(response: JSONString) {
+            self.response = response
+            self.logger = { message in print(message) }
+        }
+
+        func executeRequest(_ request: URLRequest, withSessionConfig sessionConfig: URLSessionConfiguration?) async -> Result<CleverBird.JSONString, Error> {
+            logger("Request URL: \(request.url!)")
+            if let httpBody = request.httpBody, let json = try? JSONSerialization.jsonObject(with: httpBody, options: []) {
+                logger("Request JSON:")
+                let prettyJsonData = try? JSONSerialization.data(withJSONObject: json, options: .prettyPrinted)
+                let prettyJsonStr = String(data: prettyJsonData ?? Data(), encoding: .utf8) ?? ""
+                logger(prettyJsonStr)
+            }
+            return .success(response)
+        }
+    }
+
+    func testChatCompletion() async {
+        let mockResponse = """
+        {
+            "id": "chatcmpl-6p9XYPYSTTRi0xEviKjjilqrWU2Ve",
+            "object": "chat.completion",
+            "created": 1677649420,
+            "model": "gpt-3.5-turbo",
+            "usage": {
+                "prompt_tokens": 56,
+                "completion_tokens": 31,
+                "total_tokens": 87
+            },
+            "choices": [
+                {
+                    "message": {
+                        "role": "assistant",
+                        "content": "The 2020 World Series was won by the Los Angeles Dodgers."
+                    },
+                    "finish_reason": "stop",
+                    "index": 0
+                }
+            ]
+        }
+        """
+
+        let mockURLRequester = MockURLRequester(response: mockResponse)
+
+        let openAIAPIConnection = OpenAIAPIConnection(apiKey: "your_api_key", urlRequester: mockURLRequester)
+        let chatThread = OpenAIChatThread(connection: openAIAPIConnection)
+
+        chatThread.addSystemMessage(content: "You are a helpful assistant.")
+        chatThread.addUserMessage(content: "Who won the world series in 2020?")
+
+        let completionResponse = await chatThread.complete()
+
+        XCTAssertNotNil(completionResponse, "Completion response is nil")
+        XCTAssertEqual(completionResponse?.choices.first?.message.content.trimmingCharacters(in: .whitespacesAndNewlines),
+                       "The 2020 World Series was won by the Los Angeles Dodgers.", "Unexpected assistant response")
+
+    }
+}
+
