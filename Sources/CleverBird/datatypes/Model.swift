77726eea0b1c2f00f5c9e9d4f7e6ef0744065077
B.T. Franklin <brandon.franklin@gmail.com>
2023-04-16 13:46:18 -0700

    Added custom decoding to the Model enum so it will handle responses from OpenAI correctly.

--- Sources/CleverBird/datatypes/Model.swift
+++ Sources/CleverBird/datatypes/Model.swift
@@ -1,5 +1,18 @@
 public enum Model: String, Codable {
-    case textDavinci003 = "text-davinci-003"
     case gpt35Turbo = "gpt-3.5-turbo"
     case gpt4 = "gpt-4"
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        let modelString = try container.decode(String.self)
+
+        switch modelString {
+        case _ where modelString.starts(with: "gpt-3.5"):
+            self = .gpt35Turbo
+        case _ where modelString.starts(with: "gpt-4"):
+            self = .gpt4
+        default:
+            throw DecodingError.dataCorruptedError(in: container, debugDescription: "Cannot initialize Model from invalid String value: \(modelString)")
+        }
+    }
 }
