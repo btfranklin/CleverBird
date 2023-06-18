18aef5c3e0cc19bb41104904a0549f15be2a3de2
B.T. Franklin <brandon.franklin@gmail.com>
2023-06-18 16:40:49 -0700

    Added unit tests for function call stuff.

--- Tests/CleverBirdTests/OpenAIChatThreadTests.swift
+++ Tests/CleverBirdTests/OpenAIChatThreadTests.swift
@@ -27,4 +27,56 @@ class OpenAIChatThreadTests: XCTestCase {
         XCTAssertEqual(tokenCount, 25, "Unexpected token count")
     }
 
+    func testFunctionCallMessage() throws {
+
+        let getCurrentWeatherParameters = Function.Parameters(
+            properties: [
+                "location": Function.Parameters.Property(type: .string,
+                                                         description: "The city and state, e.g. San Francisco, CA"),
+                "format": Function.Parameters.Property(type: .string,
+                                                       description: "The temperature unit to use. Infer this from the users location.",
+                                                       enumCases: ["celsius", "fahrenheit"])
+            ],
+            required: ["location", "format"])
+
+        let getCurrentWeather = Function(name: "get_current_weather",
+                                         description: "Get the current weather",
+                                         parameters: getCurrentWeatherParameters)
+
+        let getNDayWeatherForecastParameters = Function.Parameters(
+            properties: [
+                "location": Function.Parameters.Property(type: .string,
+                                                         description: "The city and state, e.g. San Francisco, CA"),
+                "format": Function.Parameters.Property(type: .string,
+                                                       description: "The temperature unit to use. Infer this from the users location.",
+                                                       enumCases: ["celsius", "fahrenheit"]),
+                "num_days": Function.Parameters.Property(type: .integer,
+                                                         description: "The number of days to forecast")
+            ],
+            required: ["location", "format", "num_days"])
+
+        let getNDayWeatherForecast = Function(name: "get_n_day_weather_forecast",
+                                              description: "Get an N-day weather forecast",
+                                              parameters: getNDayWeatherForecastParameters)
+
+        let openAIAPIConnection = OpenAIAPIConnection(apiKey: "fake_api_key")
+        let functionCall = FunctionCall(name: "testFunc", arguments: ["arg1": .string("value1")])
+        let chatThread = ChatThread(connection: openAIAPIConnection)
+            .addSystemMessage("You are a helpful assistant.")
+            .setFunctions([getCurrentWeather, getNDayWeatherForecast])
+            .addMessage(try! ChatMessage(role: .assistant, functionCall: functionCall))
+        let tokenCount = try chatThread.tokenCount()
+
+        XCTAssertEqual(tokenCount, 241, "Unexpected token count")
+    }
+
+    func testInvalidMessageCreation() {
+        let functionCall = FunctionCall(name: "testFunc", arguments: ["arg1": .string("value1")])
+        XCTAssertThrowsError(try ChatMessage(role: .assistant)) { error in
+            XCTAssertEqual(error as? CleverBirdError, CleverBirdError.invalidMessageContent)
+        }
+        XCTAssertThrowsError(try ChatMessage(role: .function, functionCall: functionCall)) { error in
+            XCTAssertEqual(error as? CleverBirdError, CleverBirdError.invalidFunctionMessage)
+        }
+    }
 }
