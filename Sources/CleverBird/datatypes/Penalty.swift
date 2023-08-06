0e69638720df1bcd6a4c18500e2a6509eef39c06
B.T. Franklin <brandon.franklin@gmail.com>
2023-08-06 16:32:27 -0700

    Fixed indentation in Percentage and Penalty.

--- Sources/CleverBird/datatypes/Penalty.swift
+++ Sources/CleverBird/datatypes/Penalty.swift
@@ -2,36 +2,36 @@ import Foundation
 
 /// Represents a value that can be between `-2.0` and `2.0`.
 public struct Penalty: Equatable {
-  public let value: Decimal
-  
-  /// Creates the penalty value, clamped between `-2.0` and `2.0`.
-  public init(_ value: Decimal) {
-    self.value = Self.clamp(value)
-  }
+    public let value: Decimal
+
+    /// Creates the penalty value, clamped between `-2.0` and `2.0`.
+    public init(_ value: Decimal) {
+        self.value = Self.clamp(value)
+    }
 }
 
 extension Penalty {
-  /// Clamps the value between `-2.0` and `2.0`.
-  public static func clamp(_ value: Decimal) -> Decimal {
-    return min(2.0, max(-2.0, value))
-  }
+    /// Clamps the value between `-2.0` and `2.0`.
+    public static func clamp(_ value: Decimal) -> Decimal {
+        return min(2.0, max(-2.0, value))
+    }
 }
 
 extension Penalty: ExpressibleByFloatLiteral {
-  /// Allows creation of a ``Penalty`` directly with a `Double`.
-  public init(floatLiteral value: Double) {
-    self.init(Decimal(value))
-  }
+    /// Allows creation of a ``Penalty`` directly with a `Double`.
+    public init(floatLiteral value: Double) {
+        self.init(Decimal(value))
+    }
 }
 
 extension Penalty: Codable {
-  public init(from decoder: Decoder) throws {
-    let container = try decoder.singleValueContainer()
-    try self.init(container.decode(Decimal.self))
-  }
-  
-  public func encode(to encoder: Encoder) throws {
-    var container = encoder.singleValueContainer()
-    try container.encode(self.value)
-  }
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        try self.init(container.decode(Decimal.self))
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encode(self.value)
+    }
 }
--- Sources/CleverBird/datatypes/Percentage.swift
+++ Sources/CleverBird/datatypes/Percentage.swift
@@ -2,36 +2,36 @@ import Foundation
 
 /// Represents a value that can be between `0.0` and `1.0`.
 public struct Percentage: Equatable {
-  public let value: Decimal
-  
-  /// Creates the percentage, clamping between `0.0` and `1.0`.
-  public init(_ value: Decimal) {
-    self.value = Self.clamp(value)
-  }
+    public let value: Decimal
+
+    /// Creates the percentage, clamping between `0.0` and `1.0`.
+    public init(_ value: Decimal) {
+        self.value = Self.clamp(value)
+    }
 }
 
 extension Percentage {
-  /// Clamps the value between `0.0` and `1.0`.
-  public static func clamp(_ value: Decimal) -> Decimal {
-    return min(1.0, max(0.0, value))
-  }
+    /// Clamps the value between `0.0` and `1.0`.
+    public static func clamp(_ value: Decimal) -> Decimal {
+        return min(1.0, max(0.0, value))
+    }
 }
 
 extension Percentage: ExpressibleByFloatLiteral {
-  /// Allows the ``Percentage`` to be created via direct assignment from a `Double`.
-  public init(floatLiteral value: Double) {
-    self.init(Decimal(value))
-  }
+    /// Allows the ``Percentage`` to be created via direct assignment from a `Double`.
+    public init(floatLiteral value: Double) {
+        self.init(Decimal(value))
+    }
 }
 
 extension Percentage: Codable {
-  public init(from decoder: Decoder) throws {
-    let container = try decoder.singleValueContainer()
-    try self.init(container.decode(Decimal.self))
-  }
-  
-  public func encode(to encoder: Encoder) throws {
-    var container = encoder.singleValueContainer()
-    try container.encode(self.value)
-  }
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        try self.init(container.decode(Decimal.self))
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encode(self.value)
+    }
 }
