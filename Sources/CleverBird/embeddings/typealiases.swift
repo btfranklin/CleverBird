8b8ce689388039bbc94487da183648656bf4c7a4
B.T. Franklin <brandon.franklin@gmail.com>
2023-09-02 16:54:53 -0700

    Added SimilarityFunction as a typealias.

--- Sources/CleverBird/embeddings/typealiases.swift
+++ Sources/CleverBird/embeddings/typealiases.swift
@@ -3,3 +3,4 @@
 public typealias Document = String
 public typealias Vector = [Double]
 public typealias Similarity = Double
+public typealias SimilarityFunction = ([Vector], Vector) -> [Similarity]
