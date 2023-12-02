b0806263c1ce31e28b0c7cff095672a16e9c393c
Ronald Mannak <ronaldmannak@me.com>
2023-12-01 21:17:39 -0800

    Make EmbeddingResponse public

--- Sources/CleverBird/embeddings/EmbeddingResponse.swift
+++ Sources/CleverBird/embeddings/EmbeddingResponse.swift
@@ -1,13 +1,13 @@
 //  Created by B.T. Franklin on 7/27/23
 
-struct EmbeddingResponse: Codable {
+public struct EmbeddingResponse: Codable {
 
-    struct EmbeddingData: Codable {
+    public struct EmbeddingData: Codable {
         let embedding: Vector
         let index: Int
     }
 
-    struct Usage: Codable {
+    public struct Usage: Codable {
         let promptTokens: Int
         let totalTokens: Int
     }
