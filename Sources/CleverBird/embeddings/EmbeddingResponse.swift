7260c776b1943e3b238ccb1de21368873e2dfbac
Ronald Mannak <ronaldmannak@me.com>
2023-12-05 08:52:35 -0800

    Make embeddings repsonse properties public

--- Sources/CleverBird/embeddings/EmbeddingResponse.swift
+++ Sources/CleverBird/embeddings/EmbeddingResponse.swift
@@ -3,13 +3,15 @@
 public struct EmbeddingResponse: Codable {
 
     public struct EmbeddingData: Codable {
-        let embedding: Vector
-        let index: Int
+        public let object: String
+        public let embedding: Vector
+        public let index: Int
     }
 
     public struct Usage: Codable {
-        let promptTokens: Int
-        let totalTokens: Int
+        public let promptTokens: Int
+        public let completionTokens: Int?
+        public let totalTokens: Int
     }
 
     public let data: [EmbeddingData]
