da201d1310354b4664a6642f9809fe90a2562bdd
Ronald Mannak <ronaldmannak@me.com>
2023-12-29 15:25:25 -0800

    Add localizedError descriptions

--- Sources/CleverBird/CleverBirdError.swift
+++ Sources/CleverBird/CleverBirdError.swift
@@ -10,3 +10,26 @@ public enum CleverBirdError: Error, Equatable {
     case invalidFunctionMessage
     case invalidEmbeddingRequest(message: String)
 }
+
+extension CleverBirdError: LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case .requestFailed(message: let message):
+            "Request failed: \(message)"
+        case .unauthorized(message: let message):
+            "Unauthorized: \(message)"
+        case .responseParsingFailed(message: let message):
+            "Parsing failed: \(message)"
+        case .tokenEncoderCreationFailed(message: let message):
+            "Encoding failed: \(message)"
+        case .tokenEncodingError(message: let message):
+            "Token encoding failed: \(message)"
+        case .invalidMessageContent:
+            "Invalid message content"
+        case .invalidFunctionMessage:
+            "Invalid function message"
+        case .invalidEmbeddingRequest(message: let message):
+            "Invalid embeddings request: \(message)"
+        }
+    }
+}
